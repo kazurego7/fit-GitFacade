23243b30dafce82cdbc80c3f3f4f9ed711714d8c
kazurego7 <kazurego7@gmail.com>
2021-01-03 19:17:28 +0900

    select の description をオプションにする

--- src/ioInterface/commonIO.ts
+++ src/ioInterface/commonIO.ts
@@ -1,5 +1,5 @@
 export interface ICommonIO {
     input(description: string): Promise<string>
-    select<T>(choices: { label: string, description: string, data: T }[]): Promise<T>
+    select<T>(choices: { label: string, description?: string, data: T }[]): Promise<T>
     output(text: string): Promise<void>
 }

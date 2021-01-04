a4a3d206a0aa57159f0c2f45f425890296b92e84
namnam <kazurego7@gmail.com>
2021-01-04 14:39:54 +0900

    Kazurego7 feat add comment (#15)
    * auto-fit/ create feature branch.
    
    * fit-featのコメント追加
    
    * fit-feat の関数コメントの修正
    
    * fit-swingのコメント追加

--- src/usecase/feat.ts
+++ src/usecase/feat.ts
@@ -3,8 +3,10 @@ import { ICommonIO } from '../ioInterface/commonIO';
 import * as service from "../service/common";
 import * as config from '../util/config';
 
+
+// featureブランチを作成する。メインブランチにチェックアウトしている場合は、作成したfeatureブランチに移動する。
 export const feat = async (io: ICommonIO, git: SimpleGit) => {
-    // feat ブランチの作成
+    // ブランチ名の取得
     const userNameOneOrArray = (await git.listConfig()).all['user.name'];
     let userName = '';
     if (Array.isArray(userNameOneOrArray)) {
@@ -15,14 +17,19 @@ export const feat = async (io: ICommonIO, git: SimpleGit) => {
     const userNameValidated = userName.replace(' ', '-');
     const branchTitle = await io.input(`ブランチのタイトルを入力してください。ブランチ名は${userNameValidated}_feat_"タイトル"となります。`);
 
+    // feat ブランチの作成
     const newBranchName = `${userNameValidated}_feat_${branchTitle}`;
     await git.branch([newBranchName, config.BRANCH_NAME_MAIN]);
+
+    // feat ブランチ作成後、空のコミットを行う(swing-stashなどのため)
     const currentBranchName = (await git.branch()).current;
     const commitMassage = `${config.COMMIT_MSG_AUTO} create feature branch.`;
     if (currentBranchName === config.BRANCH_NAME_MAIN) {
+        // メインブランチにチェックアウトしていれば、作成したfeatブランチにスイッチ
         await git.checkout([newBranchName]);
         await git.commit(commitMassage, ['--allow-empty']);
     } else {
+        // メインブランチ以外にチェックアウトしていれば、元ブランチのまま
         await service.swing(git, newBranchName);
         await git.commit(commitMassage, ['--allow-empty']);
         await service.swing(git, currentBranchName);
--- src/usecase/swing.ts
+++ src/usecase/swing.ts
@@ -3,11 +3,16 @@ import { ICommonIO } from '../ioInterface/commonIO';
 import * as config from '../util/config';
 import * as service from '../service/common';
 
+// 現在の作業を保存して、別のブランチにスイッチする。
 export const swing = async (io: ICommonIO, git: SimpleGit) => {
+    // スイッチするブランチを選択(現在のブランチ以外)
     const beforBranches = await git.branch();
     const selectableBranches = beforBranches.all
         .filter((branchName) => branchName !== beforBranches.current)
         .map((branchName) => { return { label: branchName, data: branchName }; });
     const selectedBranchName = await io.select(selectableBranches);
+
+    // 現在のワークツリーとインデックスを保存して、既存のブランチへスイッチする。
+    // スイッチ先のブランチで、そのブランチで以前に保存したワークツリーとインデックスを復元する。
     service.swing(git, selectedBranchName);
 };

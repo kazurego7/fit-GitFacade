a221cbd55d06a64371a38e3780718b5b53dc781d
kazurego7 <kazurego7@gmail.com>
2021-01-04 14:11:05 +0900

    fix

--- src/usecase/feat.ts
+++ src/usecase/feat.ts
@@ -1,5 +1,6 @@
 import { SimpleGit } from "simple-git";
 import { ICommonIO } from '../ioInterface/commonIO';
+import { swing } from "../service/common";
 import * as config from '../util/config';
 
 export const feat = async (io: ICommonIO, git: SimpleGit) => {
@@ -13,10 +14,17 @@ export const feat = async (io: ICommonIO, git: SimpleGit) => {
     }
     const userNameValidated = userName.replace(' ', '-');
     const branchTitle = await io.input(`ブランチのタイトルを入力してください。ブランチ名は${userNameValidated}_feat_"タイトル"となります。`);
+
     const newBranchName = `${userNameValidated}_feat_${branchTitle}`;
     await git.branch([newBranchName, config.BRANCH_NAME_MAIN]);
     const currentBranchName = (await git.branch()).current;
+    const commitMassage = `${config.COMMIT_MSG_AUTO} create feature branch.`;
     if (currentBranchName === config.BRANCH_NAME_MAIN) {
         await git.checkout([newBranchName]);
+        await git.commit(commitMassage, ['--allow-empty']);
+    } else {
+        await swing(git, newBranchName);
+        await git.commit(commitMassage, ['--allow-empty']);
+        await swing(git, currentBranchName);
     }
 };

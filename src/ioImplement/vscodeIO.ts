10d8dd5cd1505b7338593b922e6048f790f930ac
kazurego7 <kazurego7@gmail.com>
2021-01-10 19:14:08 +0900

    ICommonIO - message の追加 - select の choices をクラス化

--- src/ioImplement/vscodeIO.ts
+++ src/ioImplement/vscodeIO.ts
@@ -1,4 +1,4 @@
-import { ICommonIO } from "../ioInterface/commonIO";
+import { ICommonIO, SelectItem, MessageItem } from "../ioInterface/commonIO";
 import * as vscode from 'vscode';
 import { assert } from "console";
 
@@ -11,7 +11,7 @@ export class VSCodeIO implements ICommonIO {
             return text;
         }
     }
-    async select<T>(choices: { label: string; description?: string; data: T; }[]): Promise<T> {
+    async select<T>(choices: SelectItem<T>[]): Promise<T> {
         assert(choices.length > 0);
         const picked = await vscode.window.showQuickPick(choices, { ignoreFocusOut: true });
         if (picked === undefined) {
@@ -20,6 +20,22 @@ export class VSCodeIO implements ICommonIO {
             const pickedIndex = choices
                 .map((item, i) => { return { item: item, index: i }; })
                 .filter((itemIx) => itemIx.item === picked)[0];
+            return choices[pickedIndex.index].data;
+        }
+    }
+    async message<T>(message: string, choices: MessageItem<T>[]): Promise<T> {
+        assert(choices.length > 0);
+        const isDuplicatedLabels = choices.some((choice) => choices.filter((choice2) => choice2.label === choice.label).length > 1);
+        assert(!isDuplicatedLabels);
+
+        const items = choices.map((choice) => choice.label);
+        const picked = await vscode.window.showInformationMessage(message, { modal: true }, ...items);
+        if (picked === undefined) {
+            throw new Error("InfoMessage cancel.");
+        } else {
+            const pickedIndex = choices
+                .map((item, i) => { return { item: item, index: i }; })
+                .filter((itemIx) => itemIx.item.label === picked)[0];
             return choices[pickedIndex.index].data;
         }
     }
--- src/ioInterface/commonIO.ts
+++ src/ioInterface/commonIO.ts
@@ -1,5 +1,34 @@
 export interface ICommonIO {
     input(description: string): Promise<string>
-    select<T>(choices: { label: string, description?: string, data: T }[]): Promise<T>
+    select<T>(choices: SelectItem<T>[]): Promise<T>
+    message<T>(message: string, choices: MessageItem<T>[]): Promise<T>
     output(text: string): Promise<void>
+}
+
+export class SelectItem<T> {
+    label: string;
+    description?: string;
+    data: T;
+    constructor(
+        props: {
+            label: string, description?: string, data: T
+        }
+    ) {
+        this.label = props.label;
+        this.description = props.description;
+        this.data = props.data;
+    };
+}
+
+export class MessageItem<T> {
+    label: string;
+    data: T;
+    constructor(
+        props: {
+            label: string, data: T
+        }
+    ) {
+        this.label = props.label;
+        this.data = props.data;
+    };
 }

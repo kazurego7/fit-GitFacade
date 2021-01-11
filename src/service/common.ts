427717ca79453d340629ab608e989a67a0c6c1e3
kazurego7 <kazurego7@gmail.com>
2021-01-11 16:30:08 +0900

    bindStashを共通機能に追加

--- src/service/common.ts
+++ src/service/common.ts
@@ -36,6 +36,55 @@ export const getStashRevByCommitId = async (git: SimpleGit, commitId: string) =>
     }
 };
 
+/**
+ * bind stash (commit に紐付いた stash) の種類
+ */
+export enum BindStashType {
+    swing = "swing",
+    snapshot = "snapshot",
+}
+
+/**
+ * 指定された種類のbind stash の rev を全て取得する
+ * @param git
+ * @param commitId
+ */
+export const getBindStashRevs = async (git: SimpleGit, stashType: BindStashType, commitId: string) => {
+    const stashes = (await git.stash(['list'])).split('\n').map((str) => str.trim());
+    return stashes
+        .map((item) => {
+            const stashComments = item.split(': ').map((str) => str.trim());
+            return { revision: stashComments[0], message: stashComments[2] };
+        })
+        .filter((item) => {
+            return item.message === `${stashType} ${commitId}`;
+        })
+        .map((item) => item.revision);
+};
+
+/**
+ * bind stash を push する
+ * @param git 
+ * @param commitId 
+ */
+export const pushBindStash = async (git: SimpleGit, stashType: BindStashType, commitId: string) => {
+    await git.stash(['push', '--include-untracked', '--message', `${stashType} ${commitId}`]);
+};
+
+/**
+ * bind stash の内、最新のものを pop する  
+ * @param git 
+ * @param commitId 
+ */
+export const popBindStash = async (git: SimpleGit, stashType: BindStashType, commitId: string) => {
+    const stashRevs = await getBindStashRevs(git, stashType, commitId);
+    if (stashRevs.length > 0) {
+        return;
+    } else {
+        const stashRev = stashRevs[0];
+        await git.stash(['pop', '--index', stashRev]);
+    }
+};
 
 /**
  * 現在のワークツリーとインデックスを保存して、既存のブランチへスイッチする  

ffd2b675d3e0fbc5091c6094e0fb86bc8663d784
kazurego7 <a>
2021-01-17 19:44:21 +0900

    auto-fit/ create feature branch.

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import { swing } from './usecase/swing';
 import { replant } from './usecase/replant';
 import { follow } from './usecase/follow';
 import { VSCodeIO } from "./ioImplement/vscodeIO";
-import simpleGit from 'simple-git';
+import simpleGit, { SimpleGit } from 'simple-git';
 import { ICommonIO } from './ioInterface/commonIO';
 import * as path from 'path';
 import { GitExtension } from './api/git';
@@ -17,13 +17,10 @@ import * as config from './util/config';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-
-	const io: ICommonIO = new VSCodeIO();
-
 	/**
 	 * ワークスペースから、Gitのリポジトリを1つ選ばせる
 	 */
-	const chooseGit = async () => {
+	const chooseGit = async (io: ICommonIO) => {
 		const gitApi = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports?.getAPI(1);
 		if (gitApi === undefined) {
 			throw new Error("git api is not available.");
@@ -55,23 +52,36 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	};
 
-	// command event
+	/**
+	 * 簡易的なエラー表示をする
+	 */
+	const showError = async (error: Error) => {
+		vscode.window.showErrorMessage(`${error.name} : ${error.message}`);
+	};
+
+	/**
+	 * GUI の Command を作成する
+	 * @param executor usecaseの関数
+	 */
+	const createGUICommand = (executor: (io: ICommonIO, git: SimpleGit) => Promise<void>) => {
+		return async () => {
+			const io: ICommonIO = new VSCodeIO();
+			const git = await chooseGit(io);
+			try {
+				await executor(io, git);
+			} catch (e) {
+				showError(e);
+			}
+		};
+	};
+
+	// コマンドの登録
 	const handlers = [
-		vscode.commands.registerCommand('fit.setup', () => {
-			chooseGit().then((git) => setup(io, git));
-		}),
-		vscode.commands.registerCommand('fit.feat', () => {
-			chooseGit().then((git) => feat(io, git));
-		}),
-		vscode.commands.registerCommand('fit.swing', () => {
-			chooseGit().then((git) => swing(io, git));
-		}),
-		vscode.commands.registerCommand('fit.replant', () => {
-			chooseGit().then((git) => replant(io, git));
-		}),
-		vscode.commands.registerCommand('fit.follow', () => {
-			chooseGit().then((git) => follow(io, git));
-		}),
+		vscode.commands.registerCommand('fit.setup', createGUICommand(setup)),
+		vscode.commands.registerCommand('fit.feat', createGUICommand(feat)),
+		vscode.commands.registerCommand('fit.swing', createGUICommand(swing)),
+		vscode.commands.registerCommand('fit.replant', createGUICommand(replant)),
+		vscode.commands.registerCommand('fit.follow', createGUICommand(follow)),
 	];
 
 	handlers.forEach((handler, _i, _handlers) => {

9c180abe4f3f72330b7a38707461e2941b373061
kazurego7 <kazurego7@gmail.com>
2021-01-31 15:03:10 +0900

    ブランチ名のエラー文は非同期にし、エラーとなった場合は繰り返しinputを出す

--- src/usecase/feat.ts
+++ src/usecase/feat.ts
@@ -9,31 +9,33 @@ import * as config from '../util/config';
  * @param git 
  */
 export const feat = async (io: ICommonIO, git: SimpleGit) => {
-    // ブランチ名の取得
-    const newBranchNameNotice = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, `\'タイトル\'`);
-    const branchTitle = await io.input(`新しいfeatureブランチのタイトルを入力してください。ブランチ名は "${newBranchNameNotice}" となります。`);
-    if (branchTitle === undefined) {
-        return;
-    } else {
-        const invalidType = await knotBranch.validBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
-        const newBranchName = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
-        switch (invalidType) {
-            case knotBranch.Validated.blankSymbol:
-                await io.output(`ブランチ名 "${newBranchName}": ブランチシンボルが空白です。`);
-                break;
-            case knotBranch.Validated.blankTitle:
-                await io.output(`ブランチ名 "${newBranchName}": ブランチタイトルが空白です。`);
-                break;
-            case knotBranch.Validated.duplicatedBranchName:
-                await io.output(`ブランチ名 "${newBranchName}": ブランチ名が、既に存在するブランチと重複しています。`);
-                break;
-            case knotBranch.Validated.invalidFormat:
-                await io.output(`ブランチ名 "${newBranchName}": 不正なブランチ名です。`);
-                break;
-            default:
-                await knotBranch.create(git, newBranchName);
-                await io.output(`ブランチ名 "${newBranchName}": ブランチが作成されました。`);
-                break;
+    while (true) {
+        // ブランチ名の取得
+        const newBranchNameNotice = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, `\'タイトル\'`);
+        const branchTitle = await io.input(`新しいfeatureブランチのタイトルを入力してください。ブランチ名は "${newBranchNameNotice}" となります。`);
+        if (branchTitle === undefined) {
+            return;
+        } else {
+            const invalidType = await knotBranch.validBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
+            const newBranchName = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
+            switch (invalidType) {
+                case knotBranch.Validated.blankSymbol:
+                    io.output(`ブランチ名 "${newBranchName}": ブランチシンボルが空白です。`);
+                    break;
+                case knotBranch.Validated.blankTitle:
+                    io.output(`ブランチ名 "${newBranchName}": ブランチタイトルが空白です。`);
+                    break;
+                case knotBranch.Validated.duplicatedBranchName:
+                    io.output(`ブランチ名 "${newBranchName}": ブランチ名が、既に存在するブランチと重複しています。`);
+                    break;
+                case knotBranch.Validated.invalidFormat:
+                    io.output(`ブランチ名 "${newBranchName}": 不正なブランチ名です。`);
+                    break;
+                default:
+                    await knotBranch.create(git, newBranchName);
+                    io.output(`ブランチ名 "${newBranchName}": ブランチが作成されました。`);
+                    return;
+            }
         }
     }
 };
--- src/usecase/replant.ts
+++ src/usecase/replant.ts
@@ -9,38 +9,40 @@ import * as knotBranch from '../service/knotBranch';
  * @param git 
  */
 export const replant = async (io: ICommonIO, git: SimpleGit) => {
-    // 新しいブランチの名前を決定する
-    const newBranchNameNotice = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, `\'タイトル\'`);
-    const branchTitle = await io.input(`作業内容を移し替える、新しいfeatureブランチのタイトルを入力してください。ブランチ名は${newBranchNameNotice}となります。`);
-    if (branchTitle === undefined) {
-        return;
-    } else {
-        // 新しいブランチに、作業内容を移し替える
-        await git.stash(['push', '--include-untracked', '--message', 'replant']);
-        try {
-            const invalidType = await knotBranch.validBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
-            const newBranchName = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
-            switch (invalidType) {
-                case knotBranch.Validated.blankSymbol:
-                    await io.output(`ブランチ名 "${newBranchName}": ブランチシンボルが空白です。`);
-                    break;
-                case knotBranch.Validated.blankTitle:
-                    await io.output(`ブランチ名 "${newBranchName}": ブランチタイトルが空白です。`);
-                    break;
-                case knotBranch.Validated.duplicatedBranchName:
-                    await io.output(`ブランチ名 "${newBranchName}": ブランチ名が、既に存在するブランチと重複しています。`);
-                    break;
-                case knotBranch.Validated.invalidFormat:
-                    await io.output(`ブランチ名 "${newBranchName}": 不正なブランチ名です。`);
-                    break;
-                default:
-                    await knotBranch.create(git, newBranchName);
-                    await io.output(`ブランチ名 "${newBranchName}": ブランチが作成されました。`);
-                    break;
+    while (true) {
+        // 新しいブランチの名前を決定する
+        const newBranchNameNotice = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, `\'タイトル\'`);
+        const branchTitle = await io.input(`作業内容を移し替える、新しいfeatureブランチのタイトルを入力してください。ブランチ名は${newBranchNameNotice}となります。`);
+        if (branchTitle === undefined) {
+            return;
+        } else {
+            // 新しいブランチに、作業内容を移し替える
+            await git.stash(['push', '--include-untracked', '--message', 'replant']);
+            try {
+                const invalidType = await knotBranch.validBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
+                const newBranchName = await knotBranch.getBranchName(git, config.BRANCH_NAME_FEATURE_SYMBOL, branchTitle);
+                switch (invalidType) {
+                    case knotBranch.Validated.blankSymbol:
+                        io.output(`ブランチ名 "${newBranchName}": ブランチシンボルが空白です。`);
+                        break;
+                    case knotBranch.Validated.blankTitle:
+                        io.output(`ブランチ名 "${newBranchName}": ブランチタイトルが空白です。`);
+                        break;
+                    case knotBranch.Validated.duplicatedBranchName:
+                        io.output(`ブランチ名 "${newBranchName}": ブランチ名が、既に存在するブランチと重複しています。`);
+                        break;
+                    case knotBranch.Validated.invalidFormat:
+                        io.output(`ブランチ名 "${newBranchName}": 不正なブランチ名です。`);
+                        break;
+                    default:
+                        await knotBranch.create(git, newBranchName);
+                        io.output(`ブランチ名 "${newBranchName}": ブランチが作成されました。`);
+                        return;
+                }
+            } catch {
+            } finally {
+                await git.stash(['pop', '--index']);
             }
-        } catch {
-        } finally {
-            await git.stash(['pop', '--index']);
         }
     }
 };
